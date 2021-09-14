e638d0aede2c46a5781a8fea17103491c9b7c5e8
Gustavo <72715243+Gustavo-2@users.noreply.github.com>
2021-09-14 16:36:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function carregar(){
+    var msg = document.getElementById('msg');
+    var img = document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    //var hora = 19;
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if(hora >= 0 && hora <12){
+        //bom dia
+        img.src ="foto-manha.png";
+        document.body.style.background = "#d0a275"
+        
+    }else if(hora >=12 && hora <=18){
+        //boa tarde
+        img.src ='foto-tarde.png';
+        document.body.style.background = '#976230'
+    }else{
+        //boa noite
+        img.src ='foto-noite.png'
+        document.body.style.background = '#071D32'
+    }
+}
