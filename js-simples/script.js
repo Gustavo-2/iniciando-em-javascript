06c12c361593542b5d2728faf9686307a223895c
Gustavo-2 <gustavo.lf48@gmail.com>
2021-11-23 17:00:22 -0300

    adicionando o caminho para as imagens imagens/

--- js-simples/script.js
+++ js-simples/script.js
@@ -8,16 +8,16 @@ function carregar(){
 
     if(hora >= 0 && hora <12){
         //bom dia
-        img.src ="foto-manha.png";
+        img.src ="imagens/foto-manha.png";
         document.body.style.background = "#d0a275"
         
     }else if(hora >=12 && hora <=18){
         //boa tarde
-        img.src ='foto-tarde.png';
+        img.src ='imagens/foto-tarde.png';
         document.body.style.background = '#976230'
     }else{
         //boa noite
-        img.src ='foto-noite.png'
+        img.src ='imagens/foto-noite.png'
         document.body.style.background = '#071D32'
     }
 }
